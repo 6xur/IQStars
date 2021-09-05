6bc7fd903b0c330f7959db2576e85ac5ca137622
u7080308 <u7080308@anu.edu.au>
2021-09-05 19:04:07 +1000

    task 5 partly done
    Signed-off-by: u7080308 <u7080308@anu.edu.au>

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -8,8 +8,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A very simple viewer for game states in the IQ-Stars game.
@@ -35,6 +43,179 @@ public class Viewer extends Application {
      * @param gameStateString A valid game state string
      */
     void makeGameState(String gameStateString) {
+
+        Group board = new Group();
+
+        board.getChildren().clear();
+
+        // Display the blank board
+        double boardH = VIEWER_HEIGHT*2/3;
+        double boardW = boardH*1194/650;
+        ImageView blankBoard = new ImageView();
+        blankBoard.setImage(new Image("file:assets/blankBoardNumbered.png"));
+        blankBoard.setFitWidth(boardW);
+        blankBoard.setFitHeight(boardH);
+        blankBoard.setLayoutX(60);
+        blankBoard.setLayoutY((VIEWER_HEIGHT-boardH)/2);
+        board.getChildren().add(blankBoard);
+        blankBoard.toBack();
+
+        // Display images of pieces in the window
+        List<ImageView> pieces = new ArrayList<>();
+        String[] colors = {"red","orange","yellow","green","blue","indigo","pink"};
+        double[][] lengths = {{2,2.5},{2,2.5},{2,3},{3,3},{2,3},{1,3},{3,2.5}}; //first height, second width
+       for (int i = 0; i < 7; i++) {
+            ImageView image = new ImageView();
+            image.setImage(new Image("file:assets/"+colors[i]+"Piece.png"));
+            pieces.add(i,image);
+            image.setFitHeight(68.5/2*lengths[i][0]);
+            image.setFitWidth(78.5/2*lengths[i][1]);
+            image.setLayoutX(80*i);
+            image.setLayoutY(0);
+            board.getChildren().add(image);
+            image.toFront();}
+
+       // Translate the positions of each piece
+       String[] strings = gameStateString.split("W");
+       String piecePlacement = strings[0];
+       String[] colorsLetter = {"r","o","y","g","b","i","p"};
+        List<String> colorList = new ArrayList<>();
+        for (String s : colorsLetter) {
+            colorList.add(s);
+        }
+        double startX = 60+320/14;
+        double startY = 80+320/14;
+       for (int j = 0; j < piecePlacement.length(); j+=4) {
+           String piece = piecePlacement.substring(j,j+4);
+           int colorIndex = colorList.indexOf(piece.substring(0,1));
+           int orientation = Integer.parseInt(piece.substring(1,2));
+           ImageView iv = pieces.get(colorIndex);
+           iv.setFitHeight(68.5*lengths[colorIndex][0]);
+           iv.setFitWidth(78.5*lengths[colorIndex][1]);
+           int pieceX = Integer.parseInt(piece.substring(2,3));
+           int pieceY = Integer.parseInt(piece.substring(3,4));
+           double boardX = startX;
+           if (pieceY % 2 == 0) {
+               boardX = startX + pieceX * 78.5;
+           }
+           else {
+               boardX = startX + (pieceX+0.5) * 78.5;
+           }
+           double boardY = startY + pieceY * 68.5;
+           iv.setLayoutX(boardX);
+           iv.setLayoutY(boardY);
+
+           // Set orientation
+           Rotate r = new Rotate();
+           if (colorIndex == 0 || colorIndex == 5) {orientation = orientation % 3;}
+           r.setPivotX(78.5/2);
+           r.setPivotY(68.5/2);
+           r.setAngle(orientation*60);
+           iv.getTransforms().add(r);
+           double setX = iv.getLayoutX();
+           double setY = iv.getLayoutY();
+           if (orientation == 2 && (colorIndex == 0 || colorIndex == 2 || colorIndex == 6)) {
+               if (colorIndex == 6) { setX += 78.5*2; }
+               else {setX += 78.5;}
+           }
+           switch (colorIndex) {
+               case 1:
+                   switch (orientation) {
+                       case 3:
+                           setX += 78.5 * 1.5;
+                           setY += 68.5;
+                           break;
+                       case 4:
+                           setY += 68.5 * 2;
+                           break;
+                       case 5:
+                           setX -= 78.5 * 0.5;
+                           setY += 68.5;
+                           break;
+                   }
+                   break;
+               case 2:
+                   switch (orientation) {
+                       case 3:
+                           setX += 78.5 / 2;
+                           setY += 68.5 * 2;
+                           break;
+                       case 4:
+                           setX += 78.5;
+                           setY += 68.5 * 2;
+                           break;
+                       case 5:
+                           setX -= 78.5;
+                           setY += 68.5 * 2;
+                           break;
+                   }
+                   break;
+               case 3:
+                   switch (orientation) {
+                       case 3:
+                           setX += 78.5 * 2;
+                           setY += 68.5 * 2;
+                           break;
+                       case 4:
+                           setX -= 78.5 / 2;
+                           setY += 68.5 * 3;
+                           break;
+                       case 5:
+                           setX -= 78.5 / 2;
+                           setY += 68.5;
+                           break;
+                   }
+                   break;
+               case 4:
+                   switch (orientation) {
+                       case 3:
+                           setX += 78.5 * 1.5;
+                           setY += 68.5;
+                           break;
+                       case 4:
+                           setX += 78.5 ;
+                           setY += 68.5 * 2;
+                           break;
+                       case 5:
+                           setX -= 78.5 ;
+                           setY += 68.5 * 2;
+                           break;
+                   }
+                   break;
+               case 6:
+                   switch (orientation) {
+                       case 3:
+                           setX += 78.5 ;
+                           setY += 68.5 * 2;
+                           break;
+                       case 4:
+                           setY += 68.5 * 2;
+                           break;
+                       case 5:
+                           setX -= 78.5 / 2;
+                           setY += 68.5;
+                           break;
+                   }
+                   break;
+
+           }
+           iv.setLayoutX(setX);
+           iv.setLayoutY(setY);
+       }
+
+       // Wizard strings
+        if (strings.length == 2) {
+            String wizardPlacement = strings[1];
+            for (int j = 0; j < wizardPlacement.length(); j+=3) {
+                String wizardPiece = wizardPlacement.substring(j,j+3);
+                int colorIndex = colorList.indexOf(wizardPiece.substring(0,1));
+                int pieceX = Integer.parseInt(wizardPiece.substring(1,2));
+                int pieceY = Integer.parseInt(wizardPiece.substring(2,3));
+            }
+        }
+        root.getChildren().add(board);
+
+
         // FIXME Task 5 (CR): implement the simple game state viewer
     }
 
@@ -72,5 +253,6 @@ public class Viewer extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-}
+    }}
+
+
