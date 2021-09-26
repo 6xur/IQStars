2b05326b5305744d9fb8111bbced2f73e287c2be
Robert Xu <u7279920@anu.edu.au>
2021-09-26 11:47:09 +1000

    Updated OverlapsTest.java
    Signed-off-by: Robert Xu <u7279920@anu.edu.au>

--- tests/comp1110/ass2/OverlapsTest.java
+++ tests/comp1110/ass2/OverlapsTest.java
@@ -1,7 +1,6 @@
 package comp1110.ass2;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -59,7 +58,7 @@ public class OverlapsTest {
         test(orangePiece, indigoPiece, true);
         test(indigoPiece, orangePiece, true);
 
-        // A piece overlaps with another piece regardless of their orientation or colour if their top left stars are at the same location
+        // A piece overlaps with another piece regardless of their orientation or colour if their top left stars occupy the same location
         indigoPiece.setOrientationLabel(1);
         indigoPiece.setPiece(new Location(0, 0));
         test(orangePiece, indigoPiece, true);
@@ -74,15 +73,32 @@ public class OverlapsTest {
     public void testRandom() {
         Random rand = new Random();
         State[] colours = {RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, PINK};
+        Set<Location> allLocations = new HashSet<>();
+        for(int y = 0; y < 4; y++){
+            if(y % 2 == 0){
+                for(int x = 0; x < 7; x++){
+                    allLocations.add(new Location(x, y));
+                }
+            } else{
+                for(int x = 0; x < 6; x++){
+                    allLocations.add(new Location(x, y));
+                }
+            }
+        }
 
-        // A piece overlaps with all of its stars
+        // A piece only overlaps with the locations that are occupied by its stars
         State colour = colours[rand.nextInt(colours.length)];
         Piece testPiece = new Piece(colour, 0);
+        testPiece.setPiece(new Location(2, 1));
         for (Location star : testPiece.getPieceStars()) {
             test(testPiece, star, true);
         }
+        allLocations.removeAll(new HashSet<>(Arrays.asList(testPiece.getPieceStars())));
+        for(Location location : allLocations){
+            test(testPiece, location, false);
+        }
 
-        // A piece doesn't overlap with the other pieces in the solution string
+        // A piece doesn't overlap with any other piece that is in the solution string
         String solution = Games.ALL_CHALLENGES_SOLUTIONS[rand.nextInt(120)];
         ArrayList<Piece> placedPieces = IQStars.getPlacedPieces(solution);
         for (int i = 0; i < placedPieces.size(); i++) {
