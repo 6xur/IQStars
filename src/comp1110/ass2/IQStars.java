48fddcdde1e181d8eb0f496f2297cab804d094a5
u7080308 <u7080308@anu.edu.au>
2021-10-20 21:43:34 +1100

    Update game.jar

--- src/comp1110/ass2/IQStars.java
+++ src/comp1110/ass2/IQStars.java
@@ -537,11 +537,6 @@ public class IQStars {
 
         return true;
     }
-
-    public static void main(String[] args) {
-        File file = new File("bluePiece.png");
-        String path = file.getAbsolutePath();
-        System.out.println(path);
-    }
+    
 
 }
